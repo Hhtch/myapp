83e41acbecd8c1c044ca0e7497632828fb8d3b8d
Hhtch <xwisefoxx@yandex.ua>
2021-09-11 13:16:22 +0300

    почти сделал
    уже при совпадении имен не крашится, а выдет ошибку и бд закрывается

--- routes/first.js
+++ routes/first.js
@@ -46,23 +46,52 @@ router.post('/drow', function(req, res, next) {
 );
 
 router.post('/addpost', function(req, res, next) {  
-   let db = new sqlite3.Database('./base.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-      console.log('Connected to the Base database.');
-    });
-   
+  let db = new sqlite3.Database('./base.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) { console.error(err.message); }
+  console.log('Connected to the Base database.');
+  });
+  
+  let sql = `SELECT 
+              Name, Age, City               
+            FROM 
+              First
+            WHERE
+              Name = ?;`
+  let Name = `${req.body.Name}`;
+  //console.log(Name + " paBHO Name")
+    
   db.serialize(() => {
-    if (err) {
-      console.error(err.message);
-    }
+    db.get(sql, [Name], (err, row) => { 
+      if (err) {
+        return console.error(err.message);
+      }
+      return row
+      ? (
+        db.close((err) => {
+        if (err) { console.error(err.message); }
+        console.log(`Close the database connection. Такое имя уже есть ${Name}`);
+      }))      
+      : db.each(`    
+      INSERT INTO First (Name, Age, City)
+        VALUES
+          ("${req.body.Name}", ${req.body.Age}, "${req.body.City}" );
+    `); //сюда перетащить еще отправку статуса и то что бд закрыта тогда
 
-    db.each(`    
-              INSERT INTO First (Name, Age, City)
-                VALUES
-                  ("${req.body.Name}", ${req.body.Age}, "${req.body.City}" );
-            `)
+    /*  if (mysql_num_rows($result) > 0) {
+        echo 'Логин занят';
+    } else {
+        echo 'Логин свободен';
+    }*/
+/*   else if( rows > 0 )  {console.log("Имя уже есть в БД");      
+      db.close((err) => {
+        if (err) { console.error(err.message); }
+        console.log('Close the database connection.');
+      });
+    }
+    console.log("Имя свободно");*/
+  }); 
+    
+    
   });
 
   db.close((err) => {
@@ -72,6 +101,7 @@ router.post('/addpost', function(req, res, next) {
     console.log('Close the database connection.');
   });                                   
   res.sendStatus( 200 );
+
 }   
 );
 
